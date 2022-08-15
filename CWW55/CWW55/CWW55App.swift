7e4e93ecb396ea75ca758822ea6bfaf16658be06
basmah saad <basmahkw7@icloud.com>
2022-08-15 23:31:26 +0300

    go

---
+++ CWW55/CWW55/CWW55App.swift
@@ -0,0 +1,17 @@
+//
+//  CWW55App.swift
+//  CWW55
+//
+//  Created by basmah saad on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CWW55App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
